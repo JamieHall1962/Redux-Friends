31f5f6375437802b3d2508bb6ee3243ea826960d
Jamie Hall <hall.jamie@gmail.com>
2019-01-31 18:22:06 -0500

    Played with some styling. Update and Delete functionality not yet complete

--- friends/src/components/friendsList.js
+++ friends/src/components/friendsList.js
@@ -7,8 +7,8 @@ import styled from "styled-components";
 const Header = styled.div`
   background: #00274c;
   width: 30%;
-  padding: 10px 2%;
-  margin: 20px auto;
+  padding: 10px 1%;
+  margin: 10px auto;
 `;
 
 const CardText = styled.div`
@@ -16,6 +16,9 @@ const CardText = styled.div`
   letter-spacing: 1px;
   font-size: 0.9rem;
   font-weight: 400;
+  border: 3px dotted #0fa53c;
+  margin: 15px;
+  padding: 15px;
 `;
 const H2 = styled.h2`
   color: #ffcb05;
@@ -23,6 +26,28 @@ const H2 = styled.h2`
   font-size: 2rem;
   font-weight: 500;
 `;
+
+const ButtonSec = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin: 10px auto;
+  width: 100%;
+`;
+
+const Button = styled.button`
+      margin-top: 25px;   
+    font-size: 18px;
+    font-family: "Open Sans", Helvetica;
+    border-radius: 4px;
+    border-bottom: 1px solid rgba( 28, 227, 125, 0.5 );
+    background:gray;
+    color: #fff;
+    box-shadow: 0px 0px 0px rgba( 15, 165, 60, 0.1 ); 
+      
+  &:hover {
+    top: -10px;
+    box-shadow: 0px 10px 10px rgba( 15, 165, 60, 0.8 );
+  `
 
 function Friends(props) {
   return (
@@ -32,6 +57,10 @@ function Friends(props) {
           <H2> {friend.name} </H2>
           Age: {friend.age} <br />
           Email: {friend.email}
+          <ButtonSec>
+          <Button>Modify</Button>
+          <Button>Delete</Button>
+          </ButtonSec>
         </CardText>
       ))}
     </Header>
